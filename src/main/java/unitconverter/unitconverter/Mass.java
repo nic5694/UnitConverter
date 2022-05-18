13e94cc0ea0ced038760a56cc38446c6beff692e
Nic <101201789+nic5694@users.noreply.github.com>
2022-05-18 12:24:49 -0400

    Setting Mass conversion rates hashtable

--- src/main/java/unitconverter/unitconverter/Mass.java
+++ src/main/java/unitconverter/unitconverter/Mass.java
@@ -2,8 +2,16 @@ package unitconverter.unitconverter;
 
 public class Mass extends Unit{
     public Mass() {
-        //TODO set conversion table
         //ADD second conversion table (one tabble to baseline) (basleines to table)
+        conversionrates.put("mg", new LinearEquation(0,0.000001));
+        conversionrates.put("g", new LinearEquation(0,0.001));
+        conversionrates.put("kg", new LinearEquation(0,1));
+        conversionrates.put("mt", new LinearEquation(0,1000));
+        conversionrates.put("oz", new LinearEquation(0,0.02835));
+        conversionrates.put("lb", new LinearEquation(0,0.453592));
+        conversionrates.put("stone", new LinearEquation(0,6.350293));
+        conversionrates.put("st", new LinearEquation(0,907.1847));
+        conversionrates.put("lt", new LinearEquation(0,1016.047));
     }
 
 
