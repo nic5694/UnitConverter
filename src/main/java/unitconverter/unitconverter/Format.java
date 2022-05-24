c9abf1cb5a9adf79406d5aa889553c27dc5fb26a
Nic <101201789+nic5694@users.noreply.github.com>
2022-05-24 13:20:15 -0400

    Added comments and specifications in code

--- src/main/java/unitconverter/unitconverter/Format.java
+++ src/main/java/unitconverter/unitconverter/Format.java
@@ -8,6 +8,7 @@ public class Format {
     protected String formater(double value){
         double temp;
         double count = 0;
+        //getting the amount of decimal places
         if (value < 1){
             temp=value;
             while (temp<1){
@@ -15,6 +16,7 @@ public class Format {
                 count++;
             }
         }
+        //formatting the string to decide how the output will be formatted
         String f="#.##";
         for(int i=0;i<count;i++){
             f+="#";
--- src/main/java/unitconverter/unitconverter/Unit.java
+++ src/main/java/unitconverter/unitconverter/Unit.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 import java.util.Locale;
 
 public abstract class Unit implements Convertible{
-    //Protected so we have access to the hashtable from all classes that extend Unit
+    //Hashtable holding all the unit keys and conversion factors
     protected Hashtable <String,LinearEquation> conversionrates = new Hashtable<>();
 
     public Unit (){}
@@ -12,13 +12,9 @@ public abstract class Unit implements Convertible{
     //Method used by all 3 classes extending from it
     //this is the method that converts from the units given to the units desired
     public double convert(String fromUnit, String toUnit, double amount) {
-        //TODO CHECK THE ROUNDING ROUND TO MAYBE 4 TO 6 POSITIONS
         //converting the fromUnit amount to the baseline amount
         double baselineAmount=conversionrates.get(fromUnit.toLowerCase(Locale.ROOT)).evaluateToBaseline(amount);
-        //using the baseline amount to calculate the equivalence to the desired unit
-        //TODO maybe implement try catch or try catch in javaFx application
-        double finalAmount=conversionrates.get(toUnit.toLowerCase(Locale.ROOT)).evaluateFromBaseline(baselineAmount);
-        //returning the answer
-        return finalAmount;
+        //using the baseline amount to calculate the equivalence to the desired unit and returning
+        return conversionrates.get(toUnit.toLowerCase(Locale.ROOT)).evaluateFromBaseline(baselineAmount);
     }
 }
